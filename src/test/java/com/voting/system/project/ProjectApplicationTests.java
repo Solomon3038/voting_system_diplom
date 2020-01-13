e6c8e2b6e58c25bb00a9a79b8738c0be48c06159
iryna6794@gmail.com <hryzantema6794>
2020-01-13 17:51:36 +0200

    init maven spring boot starter project, add H2 properties

---
+++ src/test/java/com/voting/system/project/ProjectApplicationTests.java
@@ -0,0 +1,14 @@
+package com.voting.system.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+//https://www.baeldung.com/spring-boot-testing
+@DataJpaTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
